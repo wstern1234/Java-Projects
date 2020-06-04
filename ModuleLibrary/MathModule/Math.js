9ed0ec4450b5954b39ccc40c8dbd14ea1f1ce470
wstern1234 <58192563+wstern1234@users.noreply.github.com>
2020-06-03 19:19:34 -0700

    Create Math.js

---
+++ ModuleLibrary/MathModule/Math.js
@@ -0,0 +1,18 @@
+const Math {
+  abs = (num) => {
+    if (num < 0) {
+      num *= -1;
+    } else {
+      return num;
+    }
+  }
+  
+  pow = (num, ex) => {
+    num **= ex;
+  }
+  
+  root = (num, radIndex) => {
+    radIndex = this.pow(radIndex, -1);
+    num = this.pow(num, radIndex);
+  }
+};
