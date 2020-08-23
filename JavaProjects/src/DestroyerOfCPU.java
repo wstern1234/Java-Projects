b3f1c065e669ba69a93a4e36f1e8ab0822ded8d5
Wills Stern <willsstern@gmail.com>
2020-08-23 12:51:38 -0700

    DestroyerOfCPU added via git

---
+++ JavaProjects/src/DestroyerOfCPU.java
@@ -0,0 +1,24 @@
+//**WARNING**
+// Will turn computer into F-22 Raptor jet engine CONFIRMED. - Wills Stern
+
+public class DestroyerOfCPU {
+    public static void main(String[] args) {
+        for (int pos16 = 0; pos16 <= 9; pos16++)
+            for (int pos15 = 0; pos15 <= 9; pos15++)
+                for (int pos14 = 0; pos14 <= 9; pos14++)
+                    for (int pos13 = 0; pos13 <= 9; pos13++)
+                        for (int pos12 = 0; pos12 <= 9; pos12++)
+                            for (int pos11 = 0; pos11 <= 9; pos11++)
+                                for (int pos10 = 0; pos10 <= 9; pos10++)
+                                    for (int pos9 = 0; pos9 <= 9; pos9++)
+                                        for (int pos8 = 0; pos8 <= 9; pos8++)
+                                            for (int pos7 = 0; pos7 <= 9; pos7++)
+                                                for (int pos6 = 0; pos6 <= 9; pos6++)
+                                                    for (int pos5 = 0; pos5 <= 9; pos5++)
+                                                        for (int pos4 = 0; pos4 <= 9; pos4++)
+                                                            for (int pos3 = 0; pos3 <= 9; pos3++)
+                                                                for (int pos2 = 0; pos2 <= 9; pos2++)
+                                                                    for (int pos1 = 0; pos1 <= 9; pos1++)
+                                                                        System.out.println(Math.random());
+    }
+}
